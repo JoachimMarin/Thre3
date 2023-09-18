de3bbd50574be05aa96bffefb7ee71b9a0f3e7f2
Joachim Marin <55003364+JoachimMarin@users.noreply.github.com>
2023-09-18 10:43:32 +0200

    Updates

---
+++ assets/tiles.tsx
@@ -0,0 +1,43 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="tiles" tilewidth="256" tileheight="256" tilecount="13" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="4">
+  <image width="256" height="256" source="protective_wall.png"/>
+ </tile>
+ <tile id="1">
+  <image width="256" height="256" source="blue_wall.png"/>
+ </tile>
+ <tile id="3">
+  <image width="256" height="256" source="player.png"/>
+ </tile>
+ <tile id="2">
+  <image width="256" height="256" source="green_gun.png"/>
+ </tile>
+ <tile id="7">
+  <image width="256" height="256" source="yellow_gun.png"/>
+ </tile>
+ <tile id="6">
+  <image width="256" height="256" source="red_gun.png"/>
+ </tile>
+ <tile id="0">
+  <image width="256" height="256" source="blue_gun.png"/>
+ </tile>
+ <tile id="5">
+  <image width="256" height="256" source="purple_gun.png"/>
+ </tile>
+ <tile id="8">
+  <image width="256" height="256" source="goal.png"/>
+ </tile>
+ <tile id="9">
+  <image width="256" height="256" source="dirt_wall.png"/>
+ </tile>
+ <tile id="10">
+  <image width="256" height="256" source="mirror.png"/>
+ </tile>
+ <tile id="11">
+  <image width="256" height="256" source="shield.png"/>
+ </tile>
+ <tile id="12">
+  <image width="256" height="256" source="shovel.png"/>
+ </tile>
+</tileset>
