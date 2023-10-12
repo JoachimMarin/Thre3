de134b4675525c24269b539fcd6ab82756373650
Joachim Marin <55003364+JoachimMarin@users.noreply.github.com>
2023-10-12 12:15:04 +0200

    Add level

--- src/Constants/Definitions/LevelList.ts
+++ src/Constants/Definitions/LevelList.ts
@@ -1,5 +1,8 @@
 import Level from 'Constants/Level';
 
-const LevelList = [new Level('MirrorMirror', 'Mirror Mirror')];
+const LevelList = [
+  new Level('MirrorMirror', 'Mirror Mirror'),
+  new Level('Digging', 'Digging')
+];
 
 export default LevelList;
