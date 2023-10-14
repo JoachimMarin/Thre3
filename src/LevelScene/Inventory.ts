78909474fa50d8c75415a4d00f296ec47d37b0cc
Joachim Marin <55003364+JoachimMarin@users.noreply.github.com>
2023-10-15 01:23:33 +0200

    improve inventory performance

--- src/LevelScene/Inventory.ts
+++ src/LevelScene/Inventory.ts
@@ -2,9 +2,9 @@ import ItemType from 'Constants/ItemType';
 import SideUserInterfaceScene from './SideUserInterfaceScene';
 import * as UI from 'UserInterface';
 
-let sideUI: SideUserInterfaceScene;
-
-class VirtualInventoryEntry {
+class InventoryEntry {
+  public imageL: Phaser.GameObjects.Image;
+  public textL: UI.Text;
   public itemType: ItemType;
   public count: integer;
   public index: integer;
@@ -19,31 +19,12 @@ class VirtualInventoryEntry {
     this.itemType = itemType;
     this.count = count;
     this.index = index;
-  }
-
-  OnInventoryChange() {}
-  Update() {}
-  Remove() {}
-}
-
-class InventoryEntry extends VirtualInventoryEntry {
-  public imageL: Phaser.GameObjects.Image;
-  public textL: UI.Text;
-
-  /**
-   * Creates a new InventoryEntry.
-   * @param itemType The item type of this InventoryEntry.
-   * @param count The number of items for this item type.
-   * @param index The UI slot index. This affects the display position of the icon and text.
-   */
-  constructor(itemType: ItemType, count: integer, index: integer) {
-    super(itemType, count, index);
-    this.imageL = sideUI.add
+    this.imageL = SideUserInterfaceScene.SCENE.add
       .image(0, 0, itemType.imageKey)
       .setDisplaySize(6, 6)
       .setOrigin(0, 0);
 
-    this.textL = new UI.Text(sideUI);
+    this.textL = new UI.Text(SideUserInterfaceScene.SCENE);
     this.textL.SetSize(3);
     this.textL.GetTextObject().setOrigin(0, 0.5);
     this.OnInventoryChange();
@@ -52,13 +33,13 @@ class InventoryEntry extends VirtualInventoryEntry {
   /**
    * Updates inventory slot index and item count in the UI.
    */
-  override OnInventoryChange() {
+  OnInventoryChange() {
     const indexX = this.index % 2;
     const indexY = Math.floor(this.index / 2);
-    const x = sideUI.landscapeX(2.5 + indexX * 13);
-    const y = sideUI.landscapeY(8 + indexY * 7.5);
-    const textX = sideUI.landscapeX(8.75 + indexX * 13);
-    const textY = sideUI.landscapeY(11 + indexY * 7.5);
+    const x = SideUserInterfaceScene.SCENE.landscapeX(2.5 + indexX * 13);
+    const y = SideUserInterfaceScene.SCENE.landscapeY(8 + indexY * 7.5);
+    const textX = SideUserInterfaceScene.SCENE.landscapeX(8.75 + indexX * 13);
+    const textY = SideUserInterfaceScene.SCENE.landscapeY(11 + indexY * 7.5);
 
     this.imageL.setPosition(x, y);
     const text = this.textL.GetTextObject();
@@ -69,42 +50,37 @@ class InventoryEntry extends VirtualInventoryEntry {
   /**
    * Runs on scene update.
    */
-  override Update() {
+  Update() {
     this.textL.Update();
   }
 
   /**
    * Remove this InventoryEntry.
    */
-  override Remove() {
+  Remove() {
     this.imageL.destroy();
     this.textL.Remove();
   }
 }
 
 export default class Inventory {
-  private itemList: VirtualInventoryEntry[] = [];
-  private itemMap: Map<string, integer> = new Map<string, integer>();
+  private itemCountMap: Map<string, integer> = new Map<string, integer>();
+  private itemEntryList: InventoryEntry[] = [];
+  private itemEntryMap: Map<string, integer> = null;
+
   public readonly virtual: boolean;
 
   constructor(virtual: boolean) {
     this.virtual = virtual;
-    sideUI = SideUserInterfaceScene.SCENE;
+    if (!virtual) {
+      this.itemEntryMap = new Map<string, integer>();
+    }
   }
 
   DeepVirtualCopy() {
     const copy = new Inventory(true);
-    for (const inventoryEntry of this.itemList) {
-      copy.itemList.push(
-        new VirtualInventoryEntry(
-          inventoryEntry.itemType,
-          inventoryEntry.count,
-          inventoryEntry.count
-        )
-      );
-    }
-    for (const [key, value] of this.itemMap) {
-      copy.itemMap.set(key, value);
+    for (const [k, v] of this.itemCountMap) {
+      copy.itemCountMap.set(k, v);
     }
     return copy;
   }
@@ -112,13 +88,12 @@ export default class Inventory {
   GetKey() {
     let inventoryKey = '';
     const keys = [];
-    for (const key of this.itemMap.keys()) {
+    for (const key of this.itemCountMap.keys()) {
       keys.push(key);
     }
     keys.sort();
     for (const key of keys) {
-      inventoryKey +=
-        key + '=' + this.itemList[this.itemMap.get(key)].count + '|';
+      inventoryKey += key + '=' + this.itemCountMap.get(key) + '|';
     }
     return inventoryKey;
   }
@@ -130,19 +105,25 @@ export default class Inventory {
    */
   AddItem(itemType: ItemType, count: integer = 1) {
     const itemKey = itemType.imageKey;
-    console.log(itemKey);
-    if (!this.itemMap.has(itemKey)) {
-      const index = this.itemList.length;
-      const entry = this.virtual
-        ? new VirtualInventoryEntry(itemType, count, index)
-        : new InventoryEntry(itemType, count, index);
-      this.itemList.push(entry);
-      this.itemMap.set(itemKey, index);
-      entry.OnInventoryChange();
+
+    if (!this.itemCountMap.has(itemKey)) {
+      this.itemCountMap.set(itemKey, count);
     } else {
-      const entry = this.itemList[this.itemMap.get(itemKey)];
-      entry.count += count;
-      entry.OnInventoryChange();
+      this.itemCountMap.set(itemKey, this.itemCountMap.get(itemKey) + count);
+    }
+
+    if (!this.virtual) {
+      if (!this.itemEntryMap.has(itemKey)) {
+        const index = this.itemEntryList.length;
+        const entry = new InventoryEntry(itemType, count, index);
+        this.itemEntryList.push(entry);
+        this.itemEntryMap.set(itemKey, index);
+        entry.OnInventoryChange();
+      } else {
+        const entry = this.itemEntryList[this.itemEntryMap.get(itemKey)];
+        entry.count += count;
+        entry.OnInventoryChange();
+      }
     }
   }
 
@@ -154,23 +135,32 @@ export default class Inventory {
   RemoveItem(itemType: ItemType, count: integer = 1) {
     const itemKey = itemType.imageKey;
     const remaining = this.GetCount(itemType) - count;
-    if (remaining > 0) {
-      const entry = this.itemList[this.itemMap.get(itemKey)];
-      entry.count = remaining;
-      entry.OnInventoryChange();
-    } else if (remaining == 0) {
-      const listIndex = this.itemMap.get(itemKey);
-      this.itemList[listIndex].Remove();
-      this.itemList.splice(listIndex, 1);
-      for (let i = listIndex; i < this.itemList.length; i++) {
-        const entry = this.itemList[i];
-        entry.index = i;
-        entry.OnInventoryChange();
-        this.itemMap.set(entry.itemType.imageKey, i);
+
+    if (this.virtual) {
+      if (remaining > 0) {
+        this.itemCountMap.set(itemKey, remaining);
+      } else if (remaining == 0) {
+        this.itemCountMap.delete(itemKey);
+      } else {
+        console.error('Not enough items');
       }
-      this.itemMap.delete(itemKey);
     } else {
-      console.error('Not enough items');
+      if (remaining > 0) {
+        const entry = this.itemEntryList[this.itemEntryMap.get(itemKey)];
+        entry.count = remaining;
+        entry.OnInventoryChange();
+      } else if (remaining == 0) {
+        const listIndex = this.itemEntryMap.get(itemKey);
+        this.itemEntryList[listIndex].Remove();
+        this.itemEntryList.splice(listIndex, 1);
+        for (let i = listIndex; i < this.itemEntryList.length; i++) {
+          const entry = this.itemEntryList[i];
+          entry.index = i;
+          entry.OnInventoryChange();
+          this.itemEntryMap.set(entry.itemType.imageKey, i);
+        }
+        this.itemEntryMap.delete(itemKey);
+      }
     }
   }
 
@@ -181,15 +171,8 @@ export default class Inventory {
    */
   GetCount(itemType: ItemType) {
     const itemKey = itemType.imageKey;
-    if (this.itemMap.has(itemKey)) {
-      const listIndex = this.itemMap.get(itemKey);
-      if (this.itemList.length > listIndex) {
-        return this.itemList[listIndex].count;
-      }
-      console.error('error GetCount ' + itemKey);
-      console.log('list index = ' + listIndex);
-      console.log('list length = ' + this.itemList.length);
-      return 0;
+    if (this.itemCountMap.has(itemKey)) {
+      return this.itemCountMap.get(itemKey);
     } else {
       return 0;
     }
@@ -208,18 +191,21 @@ export default class Inventory {
    * Removes all items from the inventory.
    */
   Clear() {
-    for (const inventoryEntry of this.itemList) {
-      inventoryEntry.Remove();
+    this.itemCountMap.clear();
+    if (!this.virtual) {
+      for (const inventoryEntry of this.itemEntryList) {
+        inventoryEntry.Remove();
+      }
+      this.itemEntryList = [];
+      this.itemEntryMap.clear();
     }
-    this.itemList = [];
-    this.itemMap.clear();
   }
 
   /**
    * Runs on scene update.
    */
   Update() {
-    for (const inventoryEntry of this.itemList) {
+    for (const inventoryEntry of this.itemEntryList) {
       inventoryEntry.Update();
     }
   }
